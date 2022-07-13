e6f860ba7ea70060e97d6ba2062a7b09c9c7423a
CaptainOfPhB <captainofphb@gmail.com>
2022-07-14 00:25:25 +0800

    update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :validation_codes, only: [:create]
+      resource :session, only: [:create, :destroy]
+      resources :user, only: [:show]
+      resources :accounts
+      resources :tags
+    end
+  end
 
   # Defines the root path route ("/")
   # root "articles#index"
