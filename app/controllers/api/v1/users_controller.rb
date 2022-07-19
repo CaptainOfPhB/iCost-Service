28580140ddac8b3ffc56c46c3ba25f4d9a670a42
CaptainOfPhB <captainofphb@gmail.com>
2022-07-19 15:52:01 +0000

    WIP: create and destroy method

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,19 @@
+require "securerandom"
+
 class Api::V1::UsersController < ApplicationController
   def create
-    render json: "create user"
+    # TODO
+    # 1. validate email duplication
+    # 2. return session token if create succeed
+
+    # generate a 8-bits length user id
+    identity = SecureRandom.hex[0..7]
+    @user = User.new email: params[:email], role: :user, identity: identity, nick_name: "User_#{identity}"
+    if @user.save
+      render json: { status: :success, data: @user, errors: nil }
+    else
+      render json: { status: :error, data: nil, errors: @user.errors }
+    end
   end
 
   def update
@@ -9,6 +22,19 @@ class Api::V1::UsersController < ApplicationController
   def show
   end
 
-  def destory
+  def destroy
+    # TODO
+    # 1. if user is nil
+    # 2. if not nil, set user visible to false
+
+    return
+    # return render json: { status: :error, data: nil, errors: "User not found" } if @user.nil?
+
+    @user = User.find_by_identity params[:identity]
+    if @user.destroy
+      render json: { status: :success, data: nil, errors: nil }
+    else
+      render json: { status: :error, data: nil, errors: @user.errors }
+    end
   end
 end
