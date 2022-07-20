e5692964fa1270875c829249866e2255921ed8b8
CaptainOfPhB <captainofphb@gmail.com>
2022-07-20 16:30:24 +0000

    update user_spec

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,11 +1,11 @@
 require "rails_helper"
 
-RSpec.describe "Users", type: :request do
-  describe "GET /users" do
-    it "works! (now write some real specs)" do
+RSpec.describe "Users:", type: :request do
+  describe "POST /api/v1/user" do
+    it "can create an user" do
       fake_email = "fake_name@gmail.com"
-      get '/api/v1/accounts'
-      # post "/api/v1/user", params: { email: fake_email }
+      post "/api/v1/user", params: { email: "fake_name@gmail.com" }
+      p JSON.parse response.body
       expect(response).to have_http_status 200
     end
   end
